4eb2b81eb33b92913ae1a15cabef55b6a35150df
snsasaki <sn.sasaki@core-tech.jp>
2026-06-29 10:04:51 +0900

    :art: formのたたきを作成

--- src/Views/books/create.php
+++ src/Views/books/create.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * ★基礎課題: 新規登録フォームのビュー
  *
@@ -20,4 +21,20 @@ $title = '新規登録';
 <h2>新規書籍登録（このページを実装してください）</h2>
 
 <p class="muted">README の「基礎課題」に従って登録フォームを作成してください。</p>
+
+<form action="" method="post">
+  <td><input type="text" name="title" placeholder="タイトル"></td>
+  <td><input type="text" name="author" placeholder="著者"></td>
+  <td>
+    <select name="category_id" id="category_id">
+      <option value="技術書">技術書</option>
+      <option value="小説">小説</option>
+      <option value="ビジネス">ビジネス</option>
+      <option value="デザイン">デザイン</option>
+      <option value="専門誌">専門誌</option>
+    </select>
+  </td>
+  <td><input type="text" name="price">価格</td>
+</form>
+
 <p><a class="btn" href="/">← 一覧へ戻る</a></p>
